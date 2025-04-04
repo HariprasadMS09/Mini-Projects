e05d11d8f5893c304ef3c0d9a7a005dc9c18a6d7
Hariprasad M S <hariprasadms01@gmail.com>
2025-04-04 14:28:50 +0530

    Add files via upload

---
+++ JavaScript/practise/Learning.js
@@ -0,0 +1,9 @@
+console.log("Hello flocks, welcome to JavaScript");
+console.log("Let's start Learing");
+const student1_details = {
+    Name : "vaibhav",
+    age : 21,
+    cgpa : 9,
+    isPass : true,
+};
+student1_details[Name] = "kiran";
