6165f8a12d74f79a1672129b447b9002f5519d1d
Hariprasad M S <hariprasadms01@gmail.com>
2025-04-04 17:10:34 +0530

    Delete JavaScript/practise directory

--- JavaScript/practise/Learning.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello flocks, welcome to JavaScript");
-console.log("Let's start Learing");
-const student1_details = {
-    Name : "vaibhav",
-    age : 21,
-    cgpa : 9,
-    isPass : true,
-};
-student1_details[Name] = "kiran";
